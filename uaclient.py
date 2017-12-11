90bc2cc90543dca8d12de1693e6813167cfe4ab0
Hugo Bravo Diaz <hbravod@beta05.aulas.gsyc.urjc.es>
2017-12-11 18:06:58 +0100

    implementando archivo log

--- uaclient.py
+++ uaclient.py
@@ -5,29 +5,72 @@ Programa cliente que abre un socket a un servidor
 """
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+import os
 import socket
 import sys
-
-"""
-if len(sys.argv) < 4:
-    print("python3 uaclient.py config method option")
 
 CONFIG = sys.argv[1]
 METHOD = sys.argv[2]
 OPTION = sys.argv[3]
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER, PORT))
+def log(logfile, tipo, ip, mensaje):
+    df = open(logfile, 'a')
 
-    if method == "INVITE":
-        my_socket.send(bytes('INVITE sip:'+LINE+' SIP/2.0\r\n', 'utf-8') +
-                       b'\r\n')
+class XMLHandler(ContentHandler):
+    dicc = {}
+    def __init__(self):
 
-    if method == "BYE":
-        my_socket.send(bytes('BYE sip:'+LINE+' SIP/2.0\r\n', 'utf-8') +
-                       b'\r\n')
+        self.labels = {'account': ['username', 'psswd'],
+                       'uaserver': ['ip', 'puerto'],
+                       'rtpaudio': ['puerto'],
+                       'regproxy': ['ip', 'puerto'],
+                       'log': ['path'],
+                       'audio': ['path']
+                      }
 
+    def startElement(self, name, attrs):
+
+        if name in self.labels:
+            for atribute in self.labels[name]:
+                self.dicc[name + "_" + atribute] = attrs.get(atribute, "")
+
+    def get_tags(self):
+        return(self.dicc)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 4:
+        print("python3 uaclient.py config method option")
+
+    parser = make_parser()
+    cHandler = XMLHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open(CONFIG))
+    confdict = cHandler.get_tags()
+
+    IP_uaserver = XMLHandler.dicc['uaserver_ip']
+    PORT_uaserver = int(XMLHandler.dicc['uaserver_puerto'])
+    USER = XMLHandler.dicc['account_username']
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_socket.connect((IP_uaserver, PORT_uaserver))
+
+        print("Enviando:", OPTION)
+        if METHOD == "REGISTER":
+            mensaje = ('REGISTER sip:'+USER+' SIP/2.0\r\nExpires: ' +
+                       OPTION+'\r\n', 'utf-8')
+            my_socket.send(bytes(mensaje) + b'\r\n')
+            log(confdict['logfile'], 'sent', IP_uaserver, mensaje)
+
+        if METHOD == "INVITE":
+            my_socket.send(bytes('INVITE sip:'+OPTION+' SIP/2.0\r\n', 'utf-8') +
+                           b'\r\n')
+
+        if METHOD == "BYE":
+            my_socket.send(bytes('BYE sip:'+OPTION+' SIP/2.0\r\n', 'utf-8') +
+                           b'\r\n')
+"""
     DATA = my_socket.recv(1024)
 
     print('Recibido -- ', data.decode('utf-8'))
@@ -38,39 +81,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
                                  ' SIP/2.0\r\n', 'utf-8') + b'\r\n')
     print("Terminando socket...")
 
-print("Fin.")
+    print("Fin.")
 """
-class XMLHandler(ContentHandler):
-
-    def __init__(self):
-
-        self.list = []
-        self.labels = {'acount': ['username', 'psswd'],
-                       'uaserver': ['ip', 'puerto'],
-                       'rtpaudio': ['puerto'],
-                       'regproxy': ['ip', 'puerto'],
-                       'log': ['path'],
-                       'audio': ['path']
-                      }
-
-    def startElement(self, name, attrs):
-
-        diccionario = {}
-
-        if name in self.labels:
-            diccionario['name'] = name
-            for atribute in self.labels[name]:
-                diccionario[atribute] = attrs.get(atribute, "")
-            self.list.append(diccionario)
-
-    def get_tags(self):
-        return(self.list)
-
-
-if __name__ == "__main__":
-
-    parser = make_parser()
-    cHandler = XMLHandler()
-    parser.setContentHandler(cHandler)
-    parser.parse(open('ua1.xml'))
-    print(cHandler.get_tags())
